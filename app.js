d71ce964264b7e3cae7dd6a93d31c3541d0018e1
ERNI\cagi <gilbe.cao@gmail.com>
2020-04-19 21:01:37 +0200

    add root get value

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 
+app.get('/', (req, res) => {
+    res.send('App works!');
+});
+
 app.listen(port, () => {
     debug(`listening on port ${chalk.green(port)}`);
 });
