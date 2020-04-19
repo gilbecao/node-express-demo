41048ff62d97e2b2366bdadabd69223abe3aaea8
ERNI\cagi <gilbe.cao@gmail.com>
2020-04-19 23:55:11 +0200

    patch data

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -22,31 +22,54 @@ function router(Book) {
             });
         });
 
+    bookRouter.use('/books/:bookId', (req, res, next) => {
+        const { bookId } = req.params;
+        Book.findById(bookId, (error, book) => {
+            if (error) {
+                return res.send(error);
+            }
+            if (book) {
+                req.book = book;
+                return next();
+            }
+            return res.sendStatus(404);
+        });
+    });
     bookRouter.route('/books/:bookId')
-        .get((req, res) => {
-            const { bookId } = req.params;
-            Book.findById(bookId, (error, book) => {
+        .get((req, res) => res.send(req.book))
+        .put((req, res) => {
+            const { book } = req;
+            book.title = req.body.title;
+            book.author = req.body.author;
+            book.genre = req.body.genre;
+            book.read = req.body.rea;
+
+            req.book.save((error) => {
                 if (error) {
                     return res.send(error);
                 }
                 return res.send(book);
             });
         })
-        .put((req, res) => {
-            const { bookId } = req.params;
-            Book.findById(bookId, (error, book) => {
+        .patch((req, res) => {
+            const { book } = req;
+
+            /* eslint-disable no-underscore-dangle */
+            if (req.body._id) {
+                delete req.body._id;
+            }
+            /* eslint-enable no-underscore-dangle */
+
+            Object.entries(req.body).forEach((item) => {
+                const key = item[0];
+                const value = item[1];
+                book[key] = value;
+            });
+
+            req.book.save((error) => {
                 if (error) {
                     return res.send(error);
                 }
-
-                /* eslint-disable no-param-reassign */
-                book.title = req.body.title;
-                book.author = req.body.author;
-                book.genre = req.body.genre;
-                book.read = req.body.rea;
-                /* eslint-enable no-param-reassign */
-
-                book.save();
                 return res.send(book);
             });
         });
