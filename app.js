d7cc0cd1f1fba16e6b4fb7e7011550564ecb9c06
ERNI\cagi <gilbe.cao@gmail.com>
2020-04-19 21:37:40 +0200

    add basic router

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 
+const bookRouter = express.Router();
+bookRouter.route('/books')
+    .get((req, res) => {
+        const response = { hello: 'This is my API' };
+
+        res.json(response);
+    });
+
+app.use('/api', bookRouter);
+
 app.get('/', (req, res) => {
     res.send('App works!');
 });
